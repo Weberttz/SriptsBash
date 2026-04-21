7356743086b647cd509bf247cf55d332389d08a1
Weberttz <johngaldencio18@gmail.com>
2026-04-21 15:16:31 -0300

    feat: add exibidor-de-variaveis-de-ambiente.sh

---
+++ Exercicios03/exibidor-de-variaveis-de-ambiente.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+printf '%-10s %s\n' "USER" "$USER" 
+printf '%-10s %s\n' "HOME" "$HOME"  
+printf '%-10s %s\n' "PATH"  "$PATH"
+printf '%-10s %s\n' "SHELL" "$SHELL"
+printf '%-10s %s\n' "PWD" "$PWD"  

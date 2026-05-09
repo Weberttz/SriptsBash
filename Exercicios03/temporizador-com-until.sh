00ff2d84c7d2c9ebbc36713d1d255e32a234357e
Weberttz <johngaldencio18@gmail.com>
2026-05-08 23:34:22 -0300

    feat: add temporizador-com-until.sh e arquivos de teste

---
+++ Exercicios03/temporizador-com-until.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+[[ $# -ne 1 ]] && echo "Passe um tempo em segundos" && exit 1
+
+contador=$1
+
+until [[ $contador -eq 0 ]]; do
+    echo $contador
+    spd-say $contador
+    sleep 1
+    (( contador-- ))
+done
+
+spd-say "Concluído"
